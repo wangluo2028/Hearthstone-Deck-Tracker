194ba5cbd72483ecce123eb9782154f7b7aac534
Alexander Zeier <epikz37@gmail.com>
2016-05-10 17:28:26 +0200

    [fixup] fix upload url

--- Hearthstone Deck Tracker/HsReplay/Constants.cs
+++ Hearthstone Deck Tracker/HsReplay/Constants.cs
@@ -8,7 +8,7 @@ namespace Hearthstone_Deck_Tracker.HsReplay
 {
 	internal class Constants
 	{
-		public const string BaseUrl = "http://hsreplay.net";
+		public const string BaseUrl = "https://upload.hsreplay.net";
 		private const string BaseApi = "/api/v1";
 		private const string RawUploadApi = "/replay/upload/raw";
 		private const string GenerateUploadTokenApi = "/agents/generate_single_site_upload_token/";
