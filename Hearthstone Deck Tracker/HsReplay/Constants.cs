b28cfcc21fa1388783ac70353ba4938e80ec8d3a
Alexander Zeier <epikz37@gmail.com>
2016-05-10 15:42:57 +0200

    update url for raw log uploading

--- Hearthstone Deck Tracker/HsReplay/Constants.cs
+++ Hearthstone Deck Tracker/HsReplay/Constants.cs
@@ -10,12 +10,12 @@ namespace Hearthstone_Deck_Tracker.HsReplay
 	{
 		public const string BaseUrl = "http://hsreplay.net";
 		private const string BaseApi = "/api/v1";
-		private const string UploadApi = "/replay/upload";
+		private const string RawUploadApi = "/replay/upload/raw";
 		private const string GenerateUploadTokenApi = "/agents/generate_single_site_upload_token/";
 		private const string UploadTokenFile = "hsreplay_token";
 
 		public static string BaseApiUrl => BaseUrl + BaseApi;
-		public static string UploadUrl => BaseApiUrl + UploadApi;
+		public static string UploadUrl => BaseApiUrl + RawUploadApi;
 		public static string GenerateUploadTokenUrl => BaseApiUrl + GenerateUploadTokenApi;
 		public static string UploadTokenFilePath => Path.Combine(Config.Instance.DataDirPath, UploadTokenFile);
 	}
