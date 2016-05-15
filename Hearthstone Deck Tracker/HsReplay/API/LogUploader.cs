c6f2f84cb66f3ce2d3cfb7ab4426c79f11bf233c
Alexander Zeier <epikz37@gmail.com>
2016-05-15 10:46:47 +0200

    LogUploader: handle multiple upload requests of same log

--- Hearthstone Deck Tracker/HsReplay/API/LogUploader.cs
+++ Hearthstone Deck Tracker/HsReplay/API/LogUploader.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Web;
 using Hearthstone_Deck_Tracker.Hearthstone;
 using Hearthstone_Deck_Tracker.Stats;
 using Hearthstone_Deck_Tracker.Utility.Logging;
@@ -18,13 +17,24 @@ namespace Hearthstone_Deck_Tracker.HsReplay.API
 {
 	internal class LogUploader
 	{
+		private static readonly List<UploaderItem> InProgress = new List<UploaderItem>();
+
 		public static async Task<UploadResult> Upload(string[] logLines, GameMetaData gameMetaData, GameStats game)
 		{
-			Log.Info("Uploading...");
+			var log = string.Join(Environment.NewLine, logLines);
+			var item = new UploaderItem(log.GetHashCode());
+			if(InProgress.Contains(item))
+			{
+				Log.Info($"{item.Hash} already in progress. Waiting for it to complete...");
+				InProgress.Add(item);
+				return await item.Result;
+			}
+			InProgress.Add(item);
+			Log.Info($"Uploading {item.Hash}...");
+			UploadResult result = null;
 			try
 			{
 				var metaData = UploadMetaData.Generate(logLines, gameMetaData, game);
-				var log = string.Join(Environment.NewLine, logLines);
 				var url = UploadUrl + "?" + metaData.ToQueryString();
 				var response = await Web.PostAsync(url, log, true, ApiManager.ApiKeyHeader, await ApiManager.GetUploadTokenHeader());
 				Log.Info(response.StatusCode.ToString());
@@ -32,16 +42,22 @@ namespace Hearthstone_Deck_Tracker.HsReplay.API
 				using(var reader = new StreamReader(responseStream))
 				{
 					dynamic json = JsonConvert.DeserializeObject(reader.ReadToEnd());
+					Log.Debug(json);
 					var id = json.replay_uuid;
-					Log.Info("Success!");
-					return UploadResult.Successful(id);
+					result = UploadResult.Successful(id);
 				}
 			}
 			catch(Exception e)
 			{
 				Log.Error(e);
 			}
-			return UploadResult.Failed;
+			if(result == null)
+				result = UploadResult.Failed;
+			Log.Info($"{item.Hash} upload done: {(result.Success ? "Success" : "Failed")}");
+			foreach(var waiting in InProgress.Where(x => x.Hash == item.Hash))
+				waiting.Complete(result);
+			InProgress.RemoveAll(x => x.Hash == item.Hash);
+			return result;
 		}
 
 		public static async Task<UploadResult> FromFile(string filePath)
@@ -50,6 +66,32 @@ namespace Hearthstone_Deck_Tracker.HsReplay.API
 			using(var sr = new StreamReader(filePath))
 				content = sr.ReadToEnd();
 			return await Upload(content.Split(new []{Environment.NewLine}, StringSplitOptions.RemoveEmptyEntries).ToArray(), null, null);
+		}
+
+		public class UploaderItem
+		{
+			private readonly TaskCompletionSource<UploadResult> _tcs = new TaskCompletionSource<UploadResult>();
+
+			public int Hash { get; }
+
+			public UploaderItem(int hash)
+			{
+				Hash = hash;
+			}
+
+			public override bool Equals(object obj)
+			{
+				var uObj = obj as UploaderItem;
+				return uObj != null && Equals(uObj);
+			}
+
+			public override int GetHashCode() => Hash;
+
+			public bool Equals(UploaderItem obj) => obj.Hash == Hash;
+
+			public void Complete(UploadResult result) => _tcs.SetResult(result);
+
+			public Task<UploadResult> Result => _tcs.Task;
 		}
 	}
 }
